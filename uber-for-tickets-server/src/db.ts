46049ab80fb3f68673852857561a055f4346c650
Peter van Dijk <petervandijk@gmail.com>
2018-09-27 17:48:06 +0200

    changed dbname to default postgres

--- uber-for-tickets-server/src/db.ts
+++ uber-for-tickets-server/src/db.ts
@@ -30,7 +30,7 @@ class CustomNamingStrategy extends DefaultNamingStrategy implements NamingStrate
 export default () =>
   createConnection({
     type: "postgres",
-    url: process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/uberForTickets',
+    url: process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres',
     entities: [
       Event,
       Ticket,
